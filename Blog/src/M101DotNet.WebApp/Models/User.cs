f34e6856074149926747fb546d54327cadc230b0
itanev <iliyan.tanev@gmail.com>
2015-03-28 18:11:58 +0200

    The bases of the course project - Blog

---
+++ Blog/src/M101DotNet.WebApp/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace M101DotNet.WebApp.Models
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
