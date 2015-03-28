3691e4018b445462887694339edd19f3b61bf114
itanev <iliyan.tanev@gmail.com>
2015-03-28 18:08:30 +0200

    second homework - basic commands using C# driver

---
+++ MongoCourseHW_02_EX_02/MongoCourseHW_02_EX_02/Grade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace MongoCourseHW_02_EX_02
+{
+    public class Grade
+    {
+        public ObjectId Id { get; set; }
+        public int StudentId { get; set; }
+        public string Type { get; set; }
+        public double Score { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Id: {0}, StudentId: {1}, Type: {2}, Score: {3}", Id, StudentId, Type, Score);
+        }
+    }
+}
---
+++ MongoCourseHW_02_EX_02/MongoCourseHW_02_EX_02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace MongoCourseHW_02_EX_02
+{
+    public class Program
+    {
+        static void Main()
+        {
+            MainFunc().GetAwaiter().GetResult();
+        }
+
+        public static async Task MainFunc()
+        {
+            BsonClassMap.RegisterClassMap<Grade>(cm =>
+            {
+                cm.AutoMap();
+                cm.MapMember(x => x.StudentId).SetElementName("student_id");
+                cm.MapMember(x => x.Type).SetElementName("type");
+                cm.MapMember(x => x.Score).SetElementName("score");
+            });
+
+            const string connectionString = "mongodb://localhost:12345";
+            var client = new MongoClient(connectionString);
+
+            var db = client.GetDatabase("students");
+            var grades = db.GetCollection<Grade>("grades");
+
+            var homeworkGrades = await grades.Find(new BsonDocument("type", "homework"))
+                .SortBy(x => x.StudentId)
+                .ThenBy(x => x.Score)
+                .ToListAsync();
+
+            for (int i = 0; i < homeworkGrades.Count; i += 2)
+            {
+                var grade = homeworkGrades[i];
+                await grades.DeleteOneAsync(x => x.Id == grade.Id);
+            }
+        }
+    }
+}
